e44e615790d6881f3bb19899e52a9d0175e283de
amaragh <mercer.alecia@gmail.com>
2023-01-08 17:46:28 -0600

    set up intitial Thought schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,5 @@
 const { Schema, model } = require('mongoose');
-const reactionSchema = require('./Reaction');
+// const reactionSchema = require('./Reaction');
 
 const thoughtSchema = new Schema(
     {
@@ -8,11 +8,23 @@ const thoughtSchema = new Schema(
             required: true,
             minlength: 1,
             maxlength: 280
-        }
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        username: {
+            type: String,
+            required: true
+        },
     },
     {
         toJSON: {
             getters: true
         }
     }
-)
+);
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
