22c02f683c2a089900150c29873777ba1de24b3a
amaragh <mercer.alecia@gmail.com>
2023-01-08 21:54:22 -0600

    add CRUD for user

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
 const {
     getAllUsers,
-    createUser
+    createUser,
+    getUserById,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend
 } = require('../../controllers/user-controllers')
 
 // Set up GET all and POST at /api/users
@@ -10,4 +15,15 @@ router
     .get(getAllUsers)
     .post(createUser);
 
+// Set up GET one, PUT, and DELETE at /api/users/:id
+router
+    .route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser);
+
+router
+    .route('/:userId/friends/:friendId')
+    .put(addFriend)
+    .delete(removeFriend)
 module.exports = router;
